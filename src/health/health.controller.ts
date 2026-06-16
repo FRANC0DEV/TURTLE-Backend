b605f79a65c60617ed60b0d2812d8dc72abb1af8
Edgar Franco <leivachavezedgarfranco@gmail.com>
2026-06-15 23:11:07 -0500

    feat: add HealthController and HealthModule with health check functionality

---
+++ src/health/health.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Get } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckService,
+  HttpHealthIndicator,
+  PrismaHealthIndicator,
+} from '@nestjs/terminus';
+import { PrismaService } from '@src/prisma/prisma.service';
+
+@Controller('health')
+export class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private http: HttpHealthIndicator,
+    private prismaHealth: PrismaHealthIndicator,
+    private prismaService: PrismaService
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
+      () => this.prismaHealth.pingCheck("database",this.prismaService)
+    ]);
+  }
+}
